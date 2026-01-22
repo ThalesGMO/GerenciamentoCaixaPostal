f9e42bb7cb86cb9a137fb747bac6d1eb351f5b58
ThalesGMO <thalesoliveiramello@hotmail.com>
2026-01-22 17:19:23 -0300

    main: finalizada a arquitetura inicial do projeto

---
+++ GerenciamentoCaixaPostal.Web/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using GerenciamentoCaixaPostal.Web.Models;
+
+namespace GerenciamentoCaixaPostal.Web.Controllers;
+
+public class HomeController() : Controller
+{
+    public IActionResult Index() => View();
+}
