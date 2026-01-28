4d852c9e6fe48eedbfa93c96ec0fc95702718eee
ThalesGMO <thalesoliveiramello@hotmail.com>
2026-01-28 16:33:23 -0300

    algo irado

--- GerenciamentoCaixaPostal.Shared/Data/Context/BaseDbContext.cs
+++ GerenciamentoCaixaPostal.Shared/Data/Context/BaseDbContext.cs
@@ -1,6 +1,14 @@
+using GerenciamentoCaixaPostal.Shared.Core.Settings;
+
 namespace GerenciamentoCaixaPostal.Shared.Data.Context;
 
-public class BaseDbContext
+public class BaseDbContext(AppSettings appSettings) : Dbcontext
 {
-    
+    public override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (string.IsNullOrWhitespace(appSettings.Database.ConnectionString))
+            throw new InvaldOperationException("Connection string não está configurada");
+            
+        base.OnConfiguring(optionsBuilder);
+    }
 }
